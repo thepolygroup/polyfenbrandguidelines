d16c4bc1461c5d7bd02b68ce0117ed6280990055
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-23 20:37:01 +0200

    Content update

--- content/brand-strategy.php
+++ content/brand-strategy.php
@@ -34,7 +34,7 @@
 
   <h3 class="heading-2">2.4. Creative Concept</h3>
   <div class="preview">
-    <h4 class="heading-2">The link between business &amp; creativity</h4>
+    <h4 class="heading-2">The link between<br>business &amp; creativity</h4>
   </div>
   <p>Similar to the Keywords, but more expressive and distinctive, the Creative Concept is a word or phrase that represents the desired emotional response from the target public when interacting with the Brand.</p>
   <p>Unlike a Slogan, a Creative Concept is not necessarily developed for public use, although it can be used publicly. Its primary purpose is to be used internally as a mantra to guide the creative process when sketching a Logo, copywriting a slogan, designing an ad, or when developing any other type of Brand Communication. This ensures that all elements of the Brand will convey a consistent message.</p>
