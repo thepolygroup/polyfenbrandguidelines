11d315ec8f2ce58183cf9b217b15308b9650119c
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-23 19:05:59 +0200

    Updated content

--- content/brand-strategy.php
+++ content/brand-strategy.php
@@ -1,4 +1,61 @@
 <section id="brand-strategy">
   <h2 class="heading-1">2. Brand Strategy</h2>
 
+  <h3 class="heading-2">2.1. Voice</h3>
+  <p>The content expressed by our brand must be informative and engaging, proactive in the design community.</p>
+
+  <hr>
+
+  <h3 class="heading-2">2.2. Tone</h3>
+  <ul>
+    <li>Professional</li>
+    <li>Straight-forward</li>
+    <li>Light-hearted</li>
+    <li>Simple</li>
+    <li>Reliable</li>
+    <li>Organised</li>
+    <li>Positive</li>
+    <li>No-Frills</li>
+  </ul>
+
+  <hr>
+
+  <h3 class="heading-2">2.3. Keywords</h3>
+  <ul>
+    <li>Radical</li>
+    <li>Creative</li>
+    <li>Playful</li>
+    <li>User-friendly</li>
+    <li>Straight-forward</li>
+    <li>Clean</li>
+    <li>Distinctive</li>
+    <li>Business-focused</li>
+  </ul>
+
+  <hr>
+
+  <h3 class="heading-2">2.4. Creative Concept</h3>
+  <h4>The midpoint between business and creativity</h4>
+  <p>This concise sentence is the unifying concept that guides the branding process. Unlike a slogan, this phrase is not necessarily meant for use in public communications, but it serves as a mantra when creating the logo, choosing a typeface, designing business cards, copywriting the website’s content, or developing any other touchpoint between the brand and the audience. The creative concept ensures that all elements of the brand will consistently convey a reliable and professional message.</p>
+  <p>The core concept of our identity is that we interpret our client's business and translate it into a creative expression that speaks to the client's audience.</p>
+  <p>This creative concept should be reflected in our brand and our visual identity.</p>
+
+  <hr>
+
+  <h3 class="heading-2">2.5. Moodboard</h3>
+  <p>The Moodboard contains a collection of images the can be associated with the industry and the lifestyle of the target audience. It helps identify different colours and shapes with the aim of narrowing down a certain direction for the look-and-feel of the brand.</p>
+
+  <hr>
+
+  <h3 class="heading-2">2.6. Naming</h3>
+
+  <hr>
+
+  <h3 class="heading-2">2.7. Brand Architecture</h3>
+
+  <hr>
+
+  <h3 class="heading-2">2.8. Key Messages</h3>
+
+
 </section>
