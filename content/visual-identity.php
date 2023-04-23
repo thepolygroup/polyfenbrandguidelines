c53033833f10cb583a953ea85cdd30e397bd2ed6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-23 23:00:35 +0200

    Content update

--- content/visual-identity.php
+++ content/visual-identity.php
@@ -1,36 +1,50 @@
 <section id="visual-identity">
   <h2 class="heading-1">3. Visual Identity</h2>
 
-  <h3 class="heading-2">3.1. Logo System</h3>
+  <h3 id="logo-system" class="heading-2">3.1. Logo System</h3>
 
-  <h4 class="heading-3">3.1.1. Ideogram</h4>
+  <h4 id="ideogram" class="heading-3">3.1.1. Ideogram</h4>
   <p>The Ideogram is the visual representation of Polyfen's brand in its most basic and essential form. It's reserved for use exclusively on reduced sizes and square or circle-shaped canvases.</p>
   <p>Polyfen's Ideogram represents the conjunction of two worlds, the world of business and the creative realm. The left hemisphere of the brain (analytical), and the right hemisphere (emotional).</p>
 
-  <h4 class="heading-3">3.1.2. Emblem</h4>
+  <h4 id="emblem" class="heading-3">3.1.2. Emblem</h4>
   <p>The Emblem is the preferred visual representation of the Polyfen brand. It must be the first choice when chosing a logo variation, provided that the canvas is rectangular and the logo can be placed at an adequate size.</p>
 
-  <h4 class="heading-3">3.1.3. Wordmark</h4>
+  <h4 id="wordmark" class="heading-3">3.1.3. Wordmark</h4>
   <p>The Wordmark is the most basic logo variation for textual use. It's reserved exclusively for cases when the brand name must be written at sizes too small to fit the Emblem comforatbly.</p>
 
   <hr>
 
-  <h3 class="heading-2">3.2. Typeface Selection</h3>
+  <h3 id="typeface-selection" class="heading-2">3.2. Typeface Selection</h3>
 
-  <h4 class="heading-3">3.2.1. Primary Selection</h4>
+  <h4 id="primary-selection" class="heading-3">3.2.1. Primary Selection</h4>
   <p>Biotif is the preferred typeface for use in textual headings, buttons, and captions of all brand communications.</p>
   <a href="https://www.myfonts.com/fonts/degarism-studio/biotif/" target="_blank"><button>Preview Biotif</button></a>
   <p>Freight Text is the preferred typeface for use in body text of all brand communications.</p>
   <a href="https://www.myfonts.com/fonts/garagefonts/freight-text-pro/" target="_blank"><button>Preview Freight Text</button></a>
 
-  <h4 class="heading-3">3.2.2. Secondary Selection</h4>
+  <h4 id="secondary-selection" class="heading-3">3.2.2. Secondary Selection</h4>
   <a href="https://fonts.google.com/specimen/Merriweather+Sans" target="_blank"><button>Preview Merriweather Sans</button></a>
   <a href="https://fonts.google.com/specimen/Merriweather" target="_blank"><button>Preview Merriweather</button></a>
 
 
-  <h4 class="heading-3">3.2.3. Tertiary Selection</h4>
+  <h4 id="tertiary-selection" class="heading-3">3.2.3. Tertiary Selection</h4>
   <p>Verdana is the Teriary or "System" typeface —available in essentially every computer— selected for use in all text when neither Lato nor Source Serif are available. Since email clients accept only system fonts, for instance, all emails should be set in Verdana.</p>
   <a href="https://www.fonts.com/font/microsoft-corporation/verdana" target="_blank"><button>Preview Verdana</button></a>
+
+  <hr>
+
+  <h3 id="color-scheme" class="heading-2">3.3. Color Scheme</h3>
+
+  <h4 id="accent-color" class="heading-3">3.3.1. Accent Color</h4>
+  <p>Indigo is the primary accent hue and must be used sparingly. It's reserved exclusively for buttons, links, and other important information.</p>
+
+  <h4 id="secondary-color" class="heading-3">3.3.2. Secondary Color</h4>
+  <p>Slate is the secondary hue and can be used on backgrounds and other elements.</p>
+
+  <h4 id="grayscale" class="heading-3">3.3.3. Grayscale</h4>
+
+  <h4 id="success-and-error" class="heading-3">3.3.4. Success &amp; Error</h4>
 
 
 </section>
