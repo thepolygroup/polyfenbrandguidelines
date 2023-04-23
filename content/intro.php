cc6cfd4ada0963e60522bc607ca99c8de75430c6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-23 20:42:36 +0200

    Update intro.php

--- content/intro.php
+++ content/intro.php
@@ -1,6 +1,6 @@
 <p style="padding-top:30px;">Welcome to Polyfen's Brand Guidelines. We count on this web page to:</p>
 <ol class="hanging-list">
     <li>Centralize all our Brand Identity's documentation.</li>
-    <li>Make accessible throughout our organization all files.<br/><br/></li>
+    <li>Make accessible all Visual Identity's asset files throughout our organization.<br/><br/></li>
 </ol>
 <p>If you're just looking for our logo files to download, feel free to skip to the end: <a href="#download-assets">4. Download Assets</a>.</p>
